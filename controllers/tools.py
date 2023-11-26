6f05602eec1a02d730919a670a9c0d117eb81247
Tiko7454 <tiko7454av@gmail.com>
2023-11-27 02:05:38 +0400

    added automatic commit decorator

---
+++ controllers/tools.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import Session
+
+
+def commit_before_return(func):
+    def wrapper(db: Session, *args, **kwargs):
+        res = func(db, *args, **kwargs)
+        db.commit()
+        return res
+    return wrapper
